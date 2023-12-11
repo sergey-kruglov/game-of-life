0caa9982638d9fa1cb5bb9102f9796d6bb8165ed
Sergey Kruglov <srgy.krglv@gmail.com>
2023-12-11 08:42:02 +0200

    feat: add test code

--- main.c
+++ main.c
@@ -1,35 +1,124 @@
 #include <GLFW/glfw3.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include "vendor/glfw-3.3.8/deps/linmath.h"
 
-int main() {
+static const struct
+{
+  float x, y;
+  float r, g, b;
+} vertices[3] = {{-0.6f, -0.4f, 1.f, 0.f, 0.f},
+                 {0.6f, -0.4f, 0.f, 1.f, 0.f},
+                 {0.f, 0.6f, 0.f, 0.f, 1.f}};
+
+static const char *vertex_shader_text =
+    "#version 110\n"
+    "uniform mat4 MVP;\n"
+    "attribute vec3 vCol;\n"
+    "attribute vec2 vPos;\n"
+    "varying vec3 color;\n"
+    "void main()\n"
+    "{\n"
+    "    gl_Position = MVP * vec4(vPos, 0.0, 1.0);\n"
+    "    color = vCol;\n"
+    "}\n";
+
+static const char *fragment_shader_text =
+    "#version 110\n"
+    "varying vec3 color;\n"
+    "void main()\n"
+    "{\n"
+    "    gl_FragColor = vec4(color, 1.0);\n"
+    "}\n";
+
+void error_callback(int error, const char *description)
+{
+  fprintf(stderr, "Error: %s; ErrorCode: %d\n", description, error);
+}
+
+int main()
+{
   GLFWwindow *window;
+  GLuint vertex_buffer, vertex_shader, fragment_shader, program;
+  GLint mvp_location, vpos_location, vcol_location;
+
+  glfwSetErrorCallback(error_callback);
 
   /* Initialize the library */
-  if (!glfwInit()) return -1;
+  if (!glfwInit())
+    exit(EXIT_FAILURE);
 
   /* Create a windowed mode window and its OpenGL context */
   window = glfwCreateWindow(640, 480, "Game Of Life", NULL, NULL);
-  if (!window) {
+  if (!window)
+  {
     glfwTerminate();
-    return -1;
+    exit(EXIT_FAILURE);
   }
 
   /* Make the window's context current */
   glfwMakeContextCurrent(window);
+  glfwSwapInterval(1);
+  glfwPollEvents();
+
+  // NOTE: OpenGL error checks have been omitted for brevity
+
+  glGenBuffers(1, &vertex_buffer);
+  glBindBuffer(GL_ARRAY_BUFFER, vertex_buffer);
+  glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+  vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+  glShaderSource(vertex_shader, 1, &vertex_shader_text, NULL);
+  glCompileShader(vertex_shader);
+
+  fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+  glShaderSource(fragment_shader, 1, &fragment_shader_text, NULL);
+  glCompileShader(fragment_shader);
+
+  program = glCreateProgram();
+  glAttachShader(program, vertex_shader);
+  glAttachShader(program, fragment_shader);
+  glLinkProgram(program);
+
+  mvp_location = glGetUniformLocation(program, "MVP");
+  vpos_location = glGetAttribLocation(program, "vPos");
+  vcol_location = glGetAttribLocation(program, "vCol");
+
+  glEnableVertexAttribArray(vpos_location);
+  glVertexAttribPointer(vpos_location, 2, GL_FLOAT, GL_FALSE,
+                        sizeof(vertices[0]), (void *)0);
+  glEnableVertexAttribArray(vcol_location);
+  glVertexAttribPointer(vcol_location, 3, GL_FLOAT, GL_FALSE,
+                        sizeof(vertices[0]), (void *)(sizeof(float) * 2));
 
   /* Loop until the user closes the window */
-  while (!glfwWindowShouldClose(window)) {
-    /* Render here */
+  while (!glfwWindowShouldClose(window))
+  {
+    float ratio;
+    int width, height;
+    mat4x4 m, p, mvp;
+
+    glfwGetFramebufferSize(window, &width, &height);
+    ratio = width / (float)height;
+
+    glViewport(0, 0, width, height);
     glClear(GL_COLOR_BUFFER_BIT);
 
-    /* Swap front and back buffers */
-    glfwSwapBuffers(window);
+    mat4x4_identity(m);
+    mat4x4_rotate_Z(m, m, (float)glfwGetTime());
+    mat4x4_ortho(p, -ratio, ratio, -1.f, 1.f, 1.f, -1.f);
+    mat4x4_mul(mvp, p, m);
 
-    /* Poll for and process events */
+    glUseProgram(program);
+    glUniformMatrix4fv(mvp_location, 1, GL_FALSE, (const GLfloat *)mvp);
+    glDrawArrays(GL_TRIANGLES, 0, 3);
+
+    glfwSwapBuffers(window);
     glfwPollEvents();
   }
 
-  glfwTerminate();
+  glfwDestroyWindow(window);
 
-  return 0;
+  glfwTerminate();
+  exit(EXIT_SUCCESS);
 }
